9da2045ac580df2268e9f60b67fe59b8190d3fa0
Kaiberenn <Kaiberen@hawaii.edu>
2019-02-01 19:44:23 -1000

    Initial commit

---
+++ SwiftPlaygroundTwo.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var goodGrade="I've done all my work"
+if goodGrade == "I've done all my work"
+{
+print("I should be getting an A at this point")
+}
+
+if goodGrade == "I'm not doing very well"
+
+    //Part #2
+{
+    print("I should be getting an A at this point")
+}
+else
+{
+    print("I need to work harder in this class")
+}
+//Using boolean variable in if statements
+var gradeA = true
+
+if gradeA{
+print("I really like this class")
+}
+else
+{
+    print("I'm not so sure about coding")
+}
+
+
+
